d3532e97276c6044486e28690c78a32576809d96
Karteek Ram <karteekram@gmail.com>
2026-02-24 16:19:31 +0530

    updated code

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from PIL import Image
 import torch.nn.functional as F
 import time
 
-st.set_page_config(page_title="An Enhanced Fake Logo Verification System using Vision Transformer", layout="centered")
+st.set_page_config(page_title="An Enhanced Fake Logo", layout="centered")
 
 # ------------------- CSS -------------------
 st.markdown("""
