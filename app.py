859648fccda0d7928ad70d6ee52b555bc7c38298
Karteek Ram <karteekram@gmail.com>
2026-03-04 19:37:13 +0530

    Centered Loader Final Fix

--- app.py
+++ app.py
@@ -93,6 +93,10 @@ model = load_model()
 transform = transforms.Compose([
     transforms.Resize((224,224)),
     transforms.ToTensor()
+    transforms.Normalize(
+        mean=[0.5, 0.5, 0.5],
+        std=[0.5, 0.5, 0.5]
+    )
 ])
 
 # ------------------- IMAGE CENTER FUNCTION -------------------
